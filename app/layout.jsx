101de48bc58acc3beadeab1d62d7629a7b84fa8c
TarunJakkula <tarunjakkula1236@gmail.com>
2024-06-04 11:03:20 +0530

    added favicon.ico

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,9 @@ import Header from "./Components/Header";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={` select-none bg-[url(../public/endless-constellation.svg)] bg-black ${poppins.className}`}
       >
